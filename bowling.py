7115d32ea493e81a4bd304c30f6ea279aa7488e4
martaJesztak <jesztakm@gmail.com>
2017-11-29 15:12:13 +0100

    Added 'add_spare_result' and 'add_strike_result' functions and restructured score calculation if section for more clarity.

--- bowling.py
+++ bowling.py
@@ -2,31 +2,38 @@ def score(game):
     result = 0
     frame = 1
     first_roll = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+    for roll in range(len(game)):
+        if frame < 10:
+            if game[roll] == '/':
+                result = add_spare_result(result, roll, game, last)
+            elif game[roll] == 'X' or game[roll] == 'x':
+                result = add_strike_result(result, roll, game)
+            else:
+                result += get_value(game[roll])
+                last = get_value(game[roll])
         else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i + 1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i + 1])
-                if game[i + 2] == '/':
-                    result += 10 - get_value(game[i + 1])
-                else:
-                    result += get_value(game[i + 2])
-        last = get_value(game[i])
+            result += get_value(game[roll])
         if not first_roll:
             frame += 1
             first_roll = True
         else:
             first_roll = False
-        if game[i] == 'X' or game[i] == 'x':
+        if game[roll] == 'X' or game[roll] == 'x':
             first_roll = True
             frame += 1
     return result
 
+def add_spare_result(result, roll, game, last):
+    result += get_value(game[roll]) - last + get_value(game[roll + 1])
+    return result
+
+def add_strike_result(result, roll, game):
+    result += get_value(game[roll]) + get_value(game[roll + 1])
+    if game[roll + 2] == '/':
+        result += get_value(game[roll]) - get_value(game[roll + 1])
+    else:
+        result += get_value(game[roll + 2])
+    return result
 
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
@@ -41,3 +48,5 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+score("------------------X2/")
